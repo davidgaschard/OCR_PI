938feaa3efe25c98d2f81b68e5a73cfacdece49e
dgaschard <gaschard.david@yahoo.fr>
2014-10-13 20:56:59 +0200

    C3: Add CalculerContour function : draw a red rectangle on the number, Change im.jpg : black number 2 with white background

--- raspicam_cv_test.cpp
+++ raspicam_cv_test.cpp
@@ -7,21 +7,44 @@
 #include "opencv2/highgui/highgui.hpp"
 
 using namespace cv;
-
+Mat CalculerContour(Mat& mat1);
 int main ( int argc,char **argv ) {
-Mat mat1 = imread("im.jpg", 1);
-char d[12] = {255,0,0,0,255,0,0,0,255,0,0,0};
-Mat mat2(2,2,CV_8UC3,d);
-imwrite("img.jpg",mat2);
-std::cout << mat2<< '\n' << '\n';
-unsigned char * input = (unsigned char *)(mat2.data);
-for (int i = 0;i < mat2.rows; i++) {
-   for (int j = 0 ; j < mat2.cols; j++) {
-	std::cout << "b = " << (int)input[mat2.step*j+i] << ",";
-	std::cout << "g = " << (int)input[mat2.step*j+i+1] << ",";
-	std::cout << "r = " << (int)input[mat2.step*j+i+2] << '\n';
-   }
+	Mat mat1 = imread("im.jpg", 1);
+	char d[12] = {255,0,0,0,255,0,0,0,255,0,0,0};
+	Mat mat2(2,2,CV_8UC3,d);
+	Mat res = CalculerContour(mat1);
+	imwrite("img.jpg",res);
 }
-
-
+Mat CalculerContour(Mat& mat1)
+{
+	unsigned char * input = (unsigned char *)(mat1.data);
+	int x_min = 9999;
+	int x_max = -1;
+	int y_min = 9999;
+	int y_max = -1;
+	int b,g,r;
+	for (int i = 0 ; i < mat1.rows ; i++) { //960
+   		for (int j = 0 ; j < 3*mat1.cols ; j = j + 3) { //1280
+			b = (int)input[3*mat1.cols*i+j];
+			g = (int)input[3*mat1.cols*i+j+1];
+			r = (int)input[3*mat1.cols*i+j+2];
+			if ((b < 60) && (r < 60) && (g < 60))
+			{
+			    x_min = ((x_min > j/3)? j/3 : x_min) ;
+			    x_max = ((x_max < j/3)? j/3 : x_max) ;
+			    y_min = ((y_min > i)? i : y_min) ;
+			    y_max = ((y_max < i)? i : y_max) ;
+			}
+	   	}
+	}
+	std::cout << "x_min :" << x_min << " x_max :" << x_max << " y_min : " << y_min << " y_max : " << y_max << '\n';
+	for (int i = y_min ; i < y_max ; i++) { // 960
+		for (int j = x_min ; j < 3*x_max ; j = j + 3) { // 1280
+			input[3*mat1.cols*i+j] = 0;
+			input[3*mat1.cols*i+j+1] = 0;
+			input[3*mat1.cols*i+j+2] = 255;
+   		}
+	}
+	Mat ret(Size(1280,960),CV_8UC3, input);
+	return (ret);
 }
