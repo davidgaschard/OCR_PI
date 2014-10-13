4a04676bc7916f3f71dca10d9fe4854cbf1bf4f1
dgaschard <gaschard.david@yahoo.fr>
2014-10-13 13:30:26 +0200

    C1: Init with raspicam-0.0.6

---
+++ raspicam-0.0.6/build/utils/CMakeFiles/raspicam_cv_test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/OCR_PI/raspicam-0.0.6/utils/raspicam_cv_test.cpp" "/home/pi/OCR_PI/raspicam-0.0.6/build/utils/CMakeFiles/raspicam_cv_test.dir/raspicam_cv_test.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/OCR_PI/raspicam-0.0.6/build/src/CMakeFiles/raspicam.dir/DependInfo.cmake"
+  "/home/pi/OCR_PI/raspicam-0.0.6/build/src/CMakeFiles/raspicam_cv.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../dependencies"
+  "/usr/include/opencv"
+  "../src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
